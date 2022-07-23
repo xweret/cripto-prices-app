6dccbc79acebcd70b0de080c611af56bf5adc9f3
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-23 19:35:21 -0300

    created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
+import TableCoins from './components/TableCoins';
+import axios from 'axios';
+// descargamos a la biblioteca axios  para poder pedir la data de la api y despues la importamos
+// importamos el useEffect para que se ejecute una sola vez, usamos el objeto vacio 
 function App() {
+
+  // usamos una funcion asincrona para pedir la data de la api
+
+  const getData = async () => {
+  const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
+  console.log(res.data)
+  }
+  useEffect(() => {
+    getData()
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Coin Market </h1>
+      <TableCoins />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/TableCoins.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TableCoins = () => {
+    return (
+        <div>
+            Table
+        </div>
+
+    )
+}
+
+export default TableCoins;
