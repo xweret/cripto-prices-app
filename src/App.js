9b417d8e1a68d03f0e2b6ea870ce628658dafc47
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-24 12:53:38 -0300

    consumo de api y display en la table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TableCoins from './components/TableCoins';
 import axios from 'axios';
@@ -6,20 +6,23 @@ import axios from 'axios';
 // importamos el useEffect para que se ejecute una sola vez, usamos el objeto vacio 
 function App() {
 
+  const [coins, setCoins] = useState([]);
+
   // usamos una funcion asincrona para pedir la data de la api
 
   const getData = async () => {
   const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
   console.log(res.data)
+  setCoins(res.data)
   }
   useEffect(() => {
     getData()
 }, []);
 
   return (
-    <div className="App">
-      <h1> Coin Market </h1>
-      <TableCoins />
+    <div className="container">
+      <div className="row"><TableCoins coins ={coins} /></div>
+
     </div>
   );
 }
--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const TableCoins = () => {
+// creo al tabla con los datos de la api
+
+const TableCoins = ({coins}) => {
+
     return (
-        <div>
-            Table
-        </div>
+        <table className='table table-dark mt-4 table-hover'>
+            <thead>
+                <tr>
+                    <td>Name</td>
+                </tr>
+            </thead>
+            <tbody>
+                {coins.map(coin => (
+                    <tr key={coin.name}>
+                        <td>{coin.name}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        
 
     )
 }
